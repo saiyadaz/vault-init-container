3c4a379b7a96708b6e6cb63f90c97b5b9feb7a15
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-10 22:41:41 -0500

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 vault login $VAULT_TOKEN &>/dev/null
-vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"="$2}'
+vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"="$2}' >/data/secrets
